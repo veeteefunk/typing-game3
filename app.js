6edf525961141c3dbdec9913d9088c2443f76c94
Victor Marin <victormarinviloria@gmail.com>
2022-08-20 10:08:31 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,118 +1,13 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
+let i = 0;
+let placeholder = "";
+const txt = "example@gmail.com";
+const speed = 120;
 
-const words = [
-    'abject',
-    'aberration',
-    'abjure',
-    'abnegation',
-    'abrogate',
-    'abstract',
-    'abscond',
-    'abstruse',
-    'accede',
-    'accost',
-    'accretion', 
-    'cognizant',
-    'conflagration'
-];
-
-// Init word
-let randomWord;
-
-// Init score
-let score = 0;
-
-// Init time
-let time = 10;
-
-// set difficulty to value in ls or medium
-let difficulty = localStorage.getItem('difficulty') !== null ?
-localStorage.getItem('difficulty') : 'medium';
-
-// set difficulty select value
-difficultySelect.value = localStorage.getItem('difficulty') !== null ?
-localStorage.getItem('difficulty') : 'medium';
-
-// Focus cursor on input when page loads
-text.focus();
-
-// start counting down
-const timeInterval = setInterval(updateTime, 1000);
-
-function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
+function type() {
+    placeholder += txt.charAt(i);
+    document.getElementById('email-id').setAttribute('placeholder', placeholder);
+    i++;
+    setTimeout(type, speed);
 }
 
-function addWordToDOM() {
-    randomWord = getRandomWord();
-    word.innerHTML = randomWord;
-}
-
-function updateScore() {
-    score++;
-    scoreEl.innerHTML = score;
-}
-
-// update time
-function updateTime() {
-    time--;
-    timeEl.innerHTML = time + 's';
-
-    if (time === 0) {
-        clearInterval(timeInterval);
-        // end game
-        gameOver();
-    }
-}
-
-// game over, show end screen
-
-function gameOver() {
-    endgameEl.innerHTML = `
-        <h1>Time ran out</h1>
-        <p>Your final score is ${score}</p>
-        <button onclick="location.reload()">Reload</button>
-    `;
-    endgameEl.style.display = "flex";
-}
-
-addWordToDOM();
-
-// typing
-
-text.addEventListener('input', e => {
-    const insertedText = e.target.value;
-    if (insertedText === randomWord) {
-        addWordToDOM();
-        updateScore();
-        // clear
-        e.target.value = '';
-
-        if (difficulty === 'hard') {
-            time += 2;
-        } else if (difficulty === 'medium'){
-            time += 3;
-        } else {
-            time += 5;
-        }
-    
-        updateTime();
-    }
-})
-
-// settings btn click
-settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
-
-// settings select
-settingsForm.addEventListener('change', e => {
-    difficulty = e.target.value;
-    localStorage.setItem('difficulty', difficulty);
-})
+type();
